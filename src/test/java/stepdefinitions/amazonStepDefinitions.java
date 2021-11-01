565f01c890857a1d9ce16a6dc39fb19d32e6b85f
ragip <ragip2501@gmail.com>
2021-11-01 20:32:58 +0300

    Cucumber

---
+++ src/test/java/stepdefinitions/amazonStepDefinitions.java
@@ -0,0 +1,51 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class amazonStepDefinitions {
+    AmazonPage amazonPage=new AmazonPage();
+
+    @When("kullanici amazon sayfasina gider")
+    public void kullanici_amazon_sayfasina_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+    }
+    @When("nutella icin arama yapar")
+    public void nutella_icin_arama_yapar() {
+        amazonPage.aramaKutusu.sendKeys("Nutella"+ Keys.ENTER);
+
+    }
+    @Then("sonucun nutella icerdigini test eder")
+    public void sonucun_nutella_icerdigini_test_eder() {
+
+        Assert.assertTrue(amazonPage.sonucYazisiElementi.getText().contains("Nutella"));
+
+    }
+    @Then("sayfayi kapatir")
+    public void sayfayi_kapatir() {
+        Driver.closeDriver();
+    }
+
+    @When("java icin arama yapar")
+    public void java_icin_arama_yapar() {
+        amazonPage.aramaKutusu.sendKeys("Java"+ Keys.ENTER);
+    }
+    @Then("sonucun java icerdigini test eder")
+    public void sonucun_java_icerdigini_test_eder() {
+        Assert.assertTrue(amazonPage.sonucYazisiElementi.getText().contains("Java"));
+    }
+    @When("ipad icin arama yapar")
+    public void ipad_icin_arama_yapar() {
+        amazonPage.aramaKutusu.sendKeys("ipad"+ Keys.ENTER);
+    }
+    @Then("sonucun ipad icerdigini test eder")
+    public void sonucun_ipad_icerdigini_test_eder() {
+        Assert.assertTrue(amazonPage.sonucYazisiElementi.getText().contains("ipad"));
+    }
+
+}
